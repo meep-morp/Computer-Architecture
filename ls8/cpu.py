3f2af4cd06b472a15e2307263910e43269e346d4
meep-morp <gracieaerogirl@gmail.com>
2020-09-17 23:08:32 -0500

    Finished MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,5 +1,3 @@
-"""CPU functionality."""
-
 import sys
 
 
@@ -7,62 +5,91 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """
-            ** Construct a new CPU. **
-            What we need:
-                - RAM
-                - Register for fast memory access
-                - PC iterator
-                - Running state
-        """
-        self.running = False
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.running = False
+        self.sp = 7  # our stack pointer starts at the top of a 0-7 index
+        self.fl = 0b00000000  # all flags set to false on initialization
+        self.reg[self.sp] = 0xf4  # initialize stack pointer to RAM address f4
 
-        self.opcodes = {
+    def call_stack(self, func):
+        branch_table = {
             0b10000010: self.LDI,
             0b01000111: self.PRN,
+            0b10100010: self.MULT,
             0b00000001: self.HLT,
-            0b10100010: self.MUL,
-            0b10100011: self.DIV,
-            0b10100000: self.ADD,
-            0b10100001: self.SUB
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET
         }
-
-    def load(self):
-        """Load a program into memory."""
-
-        args = sys.argv
-
-        address = 0
-        if len(sys.argv) != 2:
-            print("Usage: python ls8.py examples/filename")
+        if func in branch_table:
+            branch_table[func]()
+        else:
+            print('invalid function')
             sys.exit(1)
 
-        try:
-            address = 0
+    def LDI(self):  # converts value to int
+        reg_num = self.ram_read(self.pc+1)
+        value = self.ram_read(self.pc+2)
+        self.reg[reg_num] = value
+        self.pc += 3
 
-            with open(sys.argv[1]) as f:
-                for line in f:
-                    t = line.split('#')
-                    n = t[0].strip()
+    def PRN(self):  # prints to console
+        reg_num = self.ram_read(self.pc+1)
+        print(self.reg[reg_num])
+        self.pc += 2
 
-                    if n == '':
-                        continue
+    def HLT(self):
+        self.running = False
+        self.pc += 1
 
-                    try:
-                        n = int(n, 2)
-                    except ValueError:
-                        print(f"Invalid number '{n}'")
-                        sys.exit(1)
+    def MULT(self):
+        self.alu('MULT', self.pc+1, self.pc+2)
+        self.pc += 3
 
-                    self.ram[address] = n
-                    address += 1
+    def PUSH(self, value=None):
+        # decrement SP
+        self.sp -= 1
+        if not value:
+            value = self.reg[self.ram_read(self.pc + 1)]
+        self.ram_write(value, self.reg[self.sp])
+        self.pc += 2
 
-        except FileNotFoundError:
-            print(f"File not found: {sys.argv[1]}")
-            sys.exit(2)
+    def POP(self):
+        value = self.ram[self.sp]
+        # go to next in ram
+        self.reg[self.ram[self.pc + 1]] = value
+        # increment sp
+        self.sp += 1
+        self.pc += 2
+
+    def CALL(self):
+        new_pc = self.reg[self.ram_read(self.pc + 1)]
+        self.PUSH(self.pc + 2)
+        self.pc = new_pc
+
+    def RET(self):
+        self.pc = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
+
+    def load(self):
+        # C:/Users/Student/Downloads/Projects/python/Computer-Architecture/ls8/examples/print8.ls8
+        file_path = sys.argv[1]
+        program = open(f"{file_path}", "r")
+        address = 0
+        for instruction in program:
+            if instruction[0] == "0" or instruction[0] == "1":
+                command = instruction.split("#", 1)[0]
+                self.ram[address] = int(command, 2)
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -70,48 +97,9 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MULT":
-            self.reg[reg_a] *= self.reg[reg_b]
-        elif op == "DIV":
-            self.reg[reg_a] //= self.reg[reg_b]
-        elif op == "SUB":
-            self.reg[reg_a] -= self.reg[reg_b]
+            self.reg[self.ram[reg_a]] *= self.reg[self.ram[reg_b]]
         else:
             raise Exception("Unsupported ALU operation")
-
-    def LDI(self):  # converts value to int
-        reg_num = self.ram_read(self.pc+1)
-        value = self.ram_read(self.pc+2)
-        self.reg[reg_num] = value
-
-    def PRN(self):  # prints to console
-        reg_num = self.ram_read(self.pc+1)
-        print(self.reg[reg_num])
-
-    def HLT(self):
-        self.running = False
-        self.pc += 1
-
-    def MUL(self):
-        self.alu('MULT', self.ram_read(self.pc + 1),
-                 self.ram_read(self.pc + 2))
-
-    def SUB(self):
-        self.alu('SUB', self.ram_read(self.pc + 1),
-                 self.ram_read(self.pc + 2))
-
-    def ADD(self):
-        self.alu('ADD', self.ram_read(self.pc + 1),
-                 self.ram_read(self.pc + 2))
-
-    def DIV(self):
-        self.alu('MULT', self.ram_read(self.pc + 1),
-                 self.ram_read(self.pc + 2))
-
-    def ram_read(self, ind):
-        return self.ram[ind]
-
-    def ram_write(self, val, ind):
-        self.ram[ind] = val
 
     def trace(self):
         """
@@ -134,19 +122,8 @@ class CPU:
         print()
 
     def run(self):
-        """
-            Run the CPU ->
-            While Loop
-        """
+        """Run the CPU."""
         self.running = True
-        self.trace()
-
         while self.running:
-            ir = self.ram_read(self.pc)
-            if ir in self.opcodes:
-                self.opcodes[ir]()
-            else:
-                print("Invalid Command")
-            number_of_operands = (ir & 0b11000000) >> 6
-            bitwise_function = number_of_operands + 1
-            self.pc += bitwise_function
+            ir = self.ram[self.pc]
+            self.call_stack(ir)
